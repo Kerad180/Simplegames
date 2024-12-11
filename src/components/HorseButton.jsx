10c9d655ef7d6da78e5ab5ba954008d79bc2286c
Dariusz Lipinski <daro180@icloud.com>
2024-12-11 17:02:22 +0100

    basic and horse buttun

---
+++ src/components/HorseButton.jsx
@@ -0,0 +1,9 @@
+import './HorseButton.css'
+
+export const HorseButton = () => {
+
+
+    return(
+        <button id="horseButton"><img src={'./src/components/black-horse.png'} ></img></button>
+    )
+}
---
+++ src/components/Maincontent.jsx
@@ -0,0 +1,13 @@
+import { HorseButton } from './HorseButton'
+import './Maincontent.css'
+
+export const Maincontent = () => {
+
+
+    return(
+        <div id="mainContent">
+            <HorseButton/>
+        </div>
+    )
+    
+}
